158120490f05f686ff1f192f1954fbc6e0550e4f
RAMAZAN MEMISOGLU (096205) <R.MEMISOGLU@THY.COM>
2020-04-21 19:12:13 +0300

    First commit

---
+++ PostCommentReply/PostCommentReply/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PostCommentReply
+//
+//  Created by Ramazan Memişoğlu on 21.04.2020.
+//  Copyright © 2020 Ramazan Memişoğlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
